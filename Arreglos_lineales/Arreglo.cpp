b687655c360beed826cd6107090733b904718bf4
Allan Peña <allanpenaguerra2003@gmail.com>
2024-03-11 00:16:28 -0600

    cambios

--- Arreglos_lineales/Arreglo.cpp
+++ Arreglos_lineales/Arreglo.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 int main(){
+
+    string nombre, apellido;
+
+    cout<<"Ingresa tu nombre y apellido: ";
+    cin>>nombre>>apellido;
+
+    cout<<" Hola "<<nombre<<" "<<apellido;
  
 
 }
